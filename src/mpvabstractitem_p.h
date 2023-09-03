646f278ca9b8e6772d4e6a6f91f6e55c2dea4286
George Florea Bănuș <georgefb899@gmail.com>
2023-09-03 17:34:05 +0300

    add license headers

--- src/mpvabstractitem_p.h
+++ src/mpvabstractitem_p.h
@@ -1,3 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2023 George Florea Bănuș <georgefb899@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+ */
+
+#ifndef MPVABSTRACTITEM_P_H_INCLUDED
+#define MPVABSTRACTITEM_P_H_INCLUDED
 
 class MpvAbstractItemPrivate
 {
@@ -14,3 +22,5 @@ public:
     mpv_render_context *m_mpv_gl{nullptr};
     QMap<QString, QVariant> m_propertiesCache;
 };
+
+#endif // MPVABSTRACTITEM_P_H_INCLUDED
--- src/mpvcontroller_p.h
+++ src/mpvcontroller_p.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2023 George Florea Bănuș <georgefb899@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+ */
+
 #ifndef MPVCONTROLLER_P_H_INCLUDED
 #define MPVCONTROLLER_P_H_INCLUDED
 
